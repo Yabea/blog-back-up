5c3858ad417e4f0ca9edfef56dd9042f755db1c9
Yabea <847511885@qq.com>
2018-03-08 19:19:33 +0800

    add photos

--- tool.py
+++ tool.py
@@ -125,7 +125,7 @@ def handle_photo():
             list_info[-1]['arr']['type'].append('image')
     list_info.reverse()  # 翻转
     final_dict = {"list": list_info}
-    with open("../Yabea.github.io/photos/data.json", "utf-8") as fp:
+    with open("../Yabea.github.io/photos/data.json", "w") as fp:
         json.dump(final_dict, fp)
 
 def cut_photo():
